a9fc55bdf27fc71c4968a955e79fec910f9077b8
miriam <miriammugure610@gmail.com>
2024-06-22 16:18:02 +0300

    add the single users API

--- src/routes/Products.routes.js
+++ src/routes/Products.routes.js
@@ -11,21 +11,19 @@ router.get("/", async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 });
-// router.get("/:id",async (req,res)=>{
-//     const id=req.params.id;
-//     try {
-//         const Result = await pool.query("SELECT * FROM Products WHERE id=$1" ,[id]);
-//         if(Result.rowCount===0){
-//             res.status(404).json({success:false ,message:"User not found"})
-//         }else{
-//             res.status(200).json({success:true,data:Result[0]})
-//         }
-//         res.json(Result);
-
-//     } catch (error) {
-//         res.status(500).json({success:false,message:error.message})
-//     }
-
-// })
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const Result = await pool.query("SELECT * FROM Products WHERE id=$1", [id]);
+    if (Result.rowCount === 0) {
+      res.status(404).json({ success: false, message: "User not found" });
+    } else {
+      res.status(200).json({ success: true, data: Result[0] });
+    }
+    res.json(Result);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
 
 export default router;
